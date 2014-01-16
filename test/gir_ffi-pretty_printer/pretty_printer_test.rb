9e142ed4de6ea01a15faa34e5f5dba13c12ce715
Matijs van Zuijlen <matijs@matijs.net>
2014-01-16 10:30:14 +0100

    Clean up unit test

--- test/gir_ffi-pretty_printer/pretty_printer_test.rb
+++ test/gir_ffi-pretty_printer/pretty_printer_test.rb
@@ -1,15 +1,10 @@
 require 'test_helper'
 describe GirFFI::PrettyPrinter do
-  describe "#initialize" do
-    it "takes no arguments" do
-      GirFFI::PrettyPrinter.new
-    end
-  end
-
   describe "#pretty_print" do
     let(:instance) { GirFFI::PrettyPrinter.new }
-    it "pretty-prints a module without type specification" do
+    it "pretty-prints a module without version specification" do
       instance.pretty_print 'GObject'
+      pass
     end
   end
 end
