ec9ca93246a8c76c06870cbae44132bc1600619e
Matijs van Zuijlen <matijs@matijs.net>
2014-10-24 21:19:12 +0200

    Refactor tests for ClassPrettyPrinter

--- test/gir_ffi-pretty_printer/class_pretty_printer_test.rb
+++ test/gir_ffi-pretty_printer/class_pretty_printer_test.rb
@@ -1,12 +1,20 @@
 require 'test_helper'
 
-class Foo
+class SimpleClass
+  def bar
+    'hello'
+  end
+end
+
+class SimpleAlias
   def bar
     'hello'
   end
 
   alias foo bar
+end
 
+class ComplexAlias
   def baz
     'original'
   end
@@ -21,53 +29,64 @@ class Foo
 end
 
 describe GirFFI::ClassPrettyPrinter do
-  let(:instance) { GirFFI::ClassPrettyPrinter.new Foo }
+  let(:instance) { GirFFI::ClassPrettyPrinter.new klass }
 
   describe "#pretty_print" do
-    it "pretty-prints the class" do
-      expected = <<-END.reset_indentation.chomp
-        class Foo < Object
+    describe "for a class with a simple method definition" do
+      let(:klass) { SimpleClass }
+      it "pretty-prints the class" do
+        expected = <<-END.reset_indentation.chomp
+          class SimpleClass < Object
 
-          def bar
-            "hello"
+            def bar
+              "hello"
+            end
           end
-          alias_method 'foo', 'bar'
-          def baz_with_qux
-            (baz_without_qux + "-more")
-          end
-          alias_method 'baz', 'baz_with_qux'
-          def baz
-            "original"
-          end
-          alias_method 'baz_without_qux', 'baz'
-        end
-      END
+        END
 
-      result = instance.pretty_print
-      result.must_equal expected
-    end
-  end
-
-  describe "#pretty_print_method" do
-    it "pretty-prints a method" do
-      expected = <<-END.reset_indentation.chomp
-        def bar
-          "hello"
-        end
-      END
-      result = instance.pretty_print_method 'bar'
-      result.must_equal expected
+        result = instance.pretty_print
+        result.must_equal expected
+      end
     end
 
-    it "pretty-prints an alias" do
-      expected = <<-END.reset_indentation.chomp
-        def bar
-          "hello"
-        end
-        alias_method 'foo', 'bar'
-      END
-      result = instance.pretty_print_method 'foo'
-      result.must_equal expected
+    describe "for a class with a simple aliased method" do
+      let(:klass) { SimpleAlias }
+      it "pretty-prints the class" do
+        expected = <<-END.reset_indentation.chomp
+          class SimpleAlias < Object
+
+            def bar
+              "hello"
+            end
+            alias_method 'foo', 'bar'
+          end
+        END
+
+        result = instance.pretty_print
+        result.must_equal expected
+      end
+    end
+
+    describe "for a class with an alias chain" do
+      let(:klass) { ComplexAlias }
+      it "pretty-prints the class" do
+        expected = <<-END.reset_indentation.chomp
+          class ComplexAlias < Object
+
+            def baz_with_qux
+              (baz_without_qux + "-more")
+            end
+            alias_method 'baz', 'baz_with_qux'
+            def baz
+              "original"
+            end
+            alias_method 'baz_without_qux', 'baz'
+          end
+        END
+
+        result = instance.pretty_print
+        result.must_equal expected
+      end
     end
   end
 end
