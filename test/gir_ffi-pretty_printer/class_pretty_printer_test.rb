737a483a546241a03169a27f4a459369c4392fe6
Matijs van Zuijlen <matijs@matijs.net>
2017-09-19 08:14:40 +0200

    Use expressive heredoc delimiters

--- test/gir_ffi-pretty_printer/class_pretty_printer_test.rb
+++ test/gir_ffi-pretty_printer/class_pretty_printer_test.rb
@@ -27,14 +27,14 @@ describe GirFFI::ClassPrettyPrinter do
     describe 'for a class with a simple method definition' do
       let(:klass) { SimpleClass }
       it 'pretty-prints the class' do
-        expected = <<-END.reset_indentation.chomp
+        expected = <<-RUBY.reset_indentation.chomp
           class SimpleClass < Object
 
             def bar
               "hello"
             end
           end
-        END
+        RUBY
 
         result = instance.pretty_print
         result.must_equal expected
@@ -44,12 +44,12 @@ describe GirFFI::ClassPrettyPrinter do
     describe 'for a class with a simple aliased method' do
       let(:klass) { SimpleAlias }
       it 'pretty-prints the class' do
-        expected = <<-END.reset_indentation.chomp
+        expected = <<-RUBY.reset_indentation.chomp
           class SimpleAlias < SimpleClass
 
             alias_method 'foo', 'bar'
           end
-        END
+        RUBY
 
         result = instance.pretty_print
         result.must_equal expected
@@ -59,7 +59,7 @@ describe GirFFI::ClassPrettyPrinter do
     describe 'for a class with an alias chain' do
       let(:klass) { ComplexAlias }
       it 'pretty-prints the class' do
-        expected = <<-END.reset_indentation.chomp
+        expected = <<-RUBY.reset_indentation.chomp
           class ComplexAlias < SimpleClass
 
             def bar_with_qux
@@ -68,7 +68,7 @@ describe GirFFI::ClassPrettyPrinter do
             alias_method 'bar', 'bar_with_qux'
             alias_method 'bar_without_qux', 'bar'
           end
-        END
+        RUBY
 
         result = instance.pretty_print
         result.must_equal expected
